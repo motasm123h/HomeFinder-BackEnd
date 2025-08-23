24f1ed5d694890372c21ed2e2d21840e7526324a
motasm123h <motasmhajeh@gmail.com>
2025-08-23 12:42:23 +0300

    update panorama images

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,20 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 
 Route::get('/panorama-image/{path}', function ($path) {
-    $fullPath = storage_path("app/public/" . $path);
-
+    // $fullPath = storage_path("app/public/" . $path);
+    $fullPath = public_path("storage" . $path);
     if (!File::exists($fullPath)) {
         abort(404);
     }
+    return $fullPath;
 
     return Response::make(File::get($fullPath), 200, [
         'Content-Type' => File::mimeType($fullPath),
         'Access-Control-Allow-Origin' => '*',
     ]);
 })->where('path', '.*');
+
+
 
 Route::get('/', function () {
     return view('welcome');
